9fc004980abb5701e9a1a0451fc2ce618639d144
KodingKat <kodingkatmc@gmail.com>
2022-12-02 02:16:27 -0500

    day 2

--- day2.cpp
+++ day2.cpp
@@ -1,46 +1,105 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <vector>
-
-#include <bits/stdc++.h>
 
 using namespace std;
-
 
 int main() {
     
     // open file
     ifstream input;
-    input.open("1.in");
+    input.open("2.in");
     if (!input.is_open()) {
         cerr << "Error Opening File\n";
         return -1;
     }
 
-    vector<unsigned int> sums;
     string line;
     unsigned int sum=0;
 
     while (getline(input, line)) {
         
-        if(line.length() == 0){
-            // cout << line << "\n";
-            // cout << sum << "\n";
-            sums.push_back(sum);
-            sum = 0;
-        }else{
-            sum += stoi(line);
+        // cout << line << "\n";
+        string O = line.substr(0,1);
+        string A = line.substr(2,1);
+
+        unsigned int score = 0;
+
+        if(A == "X"){
+            score += 1;
+            A = "A";
+        }else if(A == "Y"){
+            score += 2;
+            A = "B";
+        }else if(A == "Z"){
+            score += 3;
+            A = "C";
         }
+
+        if(O==A){
+            score += 3;
+        }else if((O=="A" && A=="B") || (O=="B" && A=="C") || (O=="C" && A=="A")){
+            score += 6;
+        }
+
+        sum += score;
 
     }
 
-    sort(sums.begin(), sums.end());
 
-    cout << "Part 1: " << sums[sums.size()-1] << "\n";
+    cout << "Part 1: " << sum << "\n";
 
-    unsigned int total = sums[sums.size()-1]+sums[sums.size()-2]+sums[sums.size()-3];
-    cout << "Part 2: " << total << "\n";
+
+    sum=0;
+    input.clear();
+    input.seekg(0);
+
+    while (getline(input, line)) {
+        
+        // cout << line << "\n";
+        string O = line.substr(0,1);
+        string A = line.substr(2,1);
+
+        unsigned int score = 0;
+
+        if(A == "X") {
+            // Loss
+            if(O == "A"){
+                score += 3;
+            }else if(O == "B"){
+                score += 1;
+            }else{
+                score += 2;
+            }
+        }else if(A == "Y"){
+            // Draw
+            score += 3;
+            if(O == "A"){
+                score += 1;
+            }else if(O == "B"){
+                score += 2;
+            }else{
+                score += 3;
+            }
+        }else{
+            // Win
+            score += 6;
+
+            if(O == "A"){
+                score += 2;
+            }else if(O == "B"){
+                score += 3;
+            }else{
+                score += 1;
+            }
+        }
+        
+
+        sum += score;
+
+    }
+
+    cout << "Part 2: " << sum << "\n";
 
 
     input.close();
