c1cdc8bcfe43f0043fe930286d5c254f8918fee8
KodingKat <kodingkatmc@gmail.com>
2022-12-16 16:43:12 -0500

    template

---
+++ template.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    
+    // open file
+    ifstream input;
+    input.open("day.in");
+    if (!input.is_open()) {
+        cerr << "Error Opening File\n";
+        return -1;
+    }
+
+
+    string line;
+    unsigned int count = 0;
+
+    while ( getline(input, line) ) {
+       
+    }
+
+    cout << "Part 1: " << count << "\n";
+
+    input.clear();
+    input.seekg(0);
+
+    input.close();
+
+    return 0;
+}
